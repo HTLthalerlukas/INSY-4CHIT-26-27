31e90ce8ba6e3b9335b3991726412e148dc70e73
NedanShade <l.thaler@htlkrems.at>
2026-09-16 08:45:35 +0200

    DataCSV
    1000000 random Values

---
+++ DataCSV/DataCSV/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RandomCsvGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "Data.csv";
+            int totalRows = 1000000;
+            Random random = new Random();
+            int stringLength = 10;
+            
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.WriteLine($"Generiere {totalRows:N0} Zeilen in '{filePath}'...");
+            
+            using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8, bufferSize: 65536))
+            {
+                for (int i = 1; i <= totalRows; i++)
+                {
+                    char[] stringBuffer = new char[stringLength];
+                    for (int j = 0; j < stringLength; j++)
+                    {
+                        stringBuffer[j] = chars[random.Next(chars.Length)];
+                    }
+                    string randomStr = new string(stringBuffer);
+                    
+                    int randomNumber = random.Next(1, 2147483647);
+                    
+                    writer.WriteLine($"{randomStr};{randomStr};{randomNumber};{randomNumber}");
+                }
+            }
+
+            Console.WriteLine("Fertig! Die Datei Data.csv wurde erfolgreich erstellt.");
+        }
+    }
+}
